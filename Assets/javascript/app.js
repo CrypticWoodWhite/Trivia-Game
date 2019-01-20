d04e495df3ac966c45f0fbed948b2b734ce7e7ab
CrypticWoodWhite <13375281+CrypticWoodWhite@users.noreply.github.com>
2019-01-19 22:28:55 -0800

    messed up original repo, second try here 1/19/19, almost done

---
+++ Assets/javascript/app.js
@@ -0,0 +1,172 @@
+// GLOBAL VARIABLES
+// list of questions and answers is inside data.js
+var guess;
+var question;
+var rightAnswer;
+var isCorrect; // boolean
+var numQuestionsAsked = 0;
+var numCorrectGuesses = 0;
+var numWrongGuesses = 0;
+var numTimeouts = 0;
+var counter = 10; // time left
+var countingDown; // countdown timer
+var timeOut; // setTimeOut
+
+// EVENT LISTENERS
+// start game
+$("#startButton").on("click", function() {
+    $("#start").remove();
+    $("#time-remaining").removeClass("hidden");
+    $("#question").removeClass("hidden");
+    $("#answers").removeClass("hidden");
+    countdown();
+    showQuestion();
+})
+
+// what happens when you click a guess
+$(".answers").on("click", function(e) {
+    guess = $(this).attr("id");
+    console.log("click! you guessed " + guess);
+    if (guess === rightAnswer) {
+        $("#message").html("<p>You guessed right, you shimmering rainbow unicorn!</p>");
+        $("#message").removeClass("hidden");
+        $("#answers").addClass("hidden");
+        $("#time-remaining").addClass("hidden");
+        numCorrectGuesses++;
+        isCorrect = true;
+    }
+    else {
+        $("#answers").addClass("hidden");
+        $("#time-remaining").addClass("hidden");
+        $("#message").html("<p>WRONG! You're a stinking pile of cubic wombat poop! The correct answer is " + rightAnswer + ".</p>");
+        $("#message").removeClass("hidden");
+        $("#time-remaining").addClass("hidden");
+        numWrongGuesses++;
+        isCorrect = false;
+    }
+})
+
+// option to start game over
+$("#restart").on("click", function() {
+    $("#end-game").addClass("hidden");
+    $("#message").addClass("hidden");
+    $("#time-remaining").removeClass("hidden");
+    $("#question").removeClass("hidden");
+    $("#answers").removeClass("hidden");
+    guess = undefined;
+    question = undefined;
+    rightAnswer = undefined;
+    isCorrect = undefined;
+    counter = 10;
+    numQuestionsAsked = 0;
+    numCorrectGuesses = 0;
+    numWrongGuesses = 0;
+    numTimeouts = 0;
+    countdown();
+    showQuestion();
+})
+
+// FUNCTIONS BELOW
+
+// shows the first question
+function showQuestion() {
+    for (i = 0; i < Object.keys(questionAnswers).length; i++) {
+        $.each(questionAnswers, function(i, value) {
+            question = questionAnswers[i].question.val;
+            rightAnswer = questionAnswers.i.correctAnswer.val;
+            $("#question").html(question);
+            $("#answer-A").html(questionAnswers[i].answers.answerA.val);
+            $("#answer-B").html(questionAnswers[i].answers.answerB.val);
+            $("#answer-C").html(questionAnswers[i].answers.answerC.val);
+            $("#answer-D").html(questionAnswers[i].answers.answerD.val);
+        })
+    }
+}
+
+// all following questions
+function nextQuestion() {
+    isCorrect = undefined;
+    resetTimer();
+}
+
+function resetTimer() {
+    counter = 10;
+    timeout = setTimeout(function() {
+        $("#message").addClass("hidden");
+        $("#answers").removeClass("hidden");
+        $("#time-remaining").removeClass("hidden");
+        countdown();
+        },
+        3000
+    );
+}
+
+function countdown() {
+    counter = 10;
+    countingDown = setInterval(function() {
+        $("#timer").html("Time remaining: " + counter + " seconds");
+        if (counter < 0) {
+            clearInterval(countingDown);
+            numQuestionsAsked++;
+            numTimeouts++;
+            console.log("#timeouts: " + numTimeouts);
+            if (numQuestionsAsked === Object.keys(questionAnswers).length) {
+                console.log("game over");
+                gameOver();
+            }
+            else {
+                timeoutMessage();
+                nextQuestion();
+            }
+        }
+        else if (isCorrect === true && counter > 0) {
+            console.log("good guess");
+            clearInterval(countingDown);
+            numQuestionsAsked++;
+            console.log("#correct guesses: " + numCorrectGuesses);
+            if (numQuestionsAsked === Object.keys(questionAnswers).length) {
+                console.log("game over");
+                gameOver();
+            }
+            else {
+                nextQuestion();
+            }
+            //functions
+        }
+        else if (isCorrect === false && counter > 0) {
+            console.log("wrong guess");
+            clearInterval(countingDown);
+            numQuestionsAsked++;
+            console.log("#wrong guesses: " + numWrongGuesses);
+            if (numQuestionsAsked === Object.keys(questionAnswers).length) {
+                console.log("game over");
+                gameOver();
+            }
+            else {
+                nextQuestion();
+            }
+            // functions
+        }
+        counter--;
+    },1000);
+}
+
+function timeoutMessage() {
+    $("#answers").addClass("hidden");
+    $("#time-remaining").addClass("hidden");
+    $("#message").html("<p>You ran out of time, you indecisive garden snail! The correct answer is " + rightAnswer + ".</p>");
+    $("#message").removeClass("hidden");
+}
+
+// shows result at end of game
+function gameOver() {
+    isCorrect = undefined;
+    counter = 11;
+    $("#time-remaining").addClass("hidden");
+    $("#question").addClass("hidden");
+    $("#answers").addClass("hidden");
+    $("#number-correct-guesses").html("Correct guesses: " + numCorrectGuesses);
+    $("#number-wrong-guesses").html("Wrong guesses: " + numWrongGuesses);
+    $("#number-timeouts").html("Timeouts: " + numTimeouts);
+    $("#end-game").removeClass("hidden");
+}
---
+++ Assets/javascript/data.js
@@ -0,0 +1,202 @@
+var questionAnswers = {
+    one: {
+        question: "In the year 1900 in the U.S. what were the most popular first names given to boy and girl babies?",
+        correctAnswer: "C. John and Mary",
+        answers: {
+            answerA: "A. William and Elizabeth",
+            answerB: "B. Joseph and Catherine",
+            answerC: "C. John and Mary",
+            answerD: "D. George and Anne"
+        }
+    },
+    two: {
+        question: "When did the Liberty Bell get its name?",
+        correctAnswer: "C. in the 19th century, when it became a symbol of the abolition of slavery",
+        answers: {
+            answerA: "A. when it was made, in 1701",
+            answerB: "B. when it rang on July 4, 1776",
+            answerC: "C. in the 19th century, when it became a symbol of the abolition of slavery",
+            answerD: "D. none of the above"
+        }
+    },
+    three: {
+        question: "In the Roy Rogers -Dale Evans Museum, you will find Roy and Dales stuffed horses. Roy's horse was named Trigger, which was Dales horse?",
+        correctAnswer: "A. Buttermilk",
+        answers: {
+            answerA: "A. Buttermilk",
+            answerB: "B. Daisy",
+            answerC: "C. Scout",
+            answerD: "D. Tulip"
+        }
+    },
+    four: {
+        question: "The Daniel Boon museum at the home where he died can best be described how?",
+        correctAnswer: "C. a four-story Georgian-style home in Missouri",
+        answers: {
+            answerA: "A. a log cabin in Kentucky",
+            answerB: "B. a two-story clapboard house in Tennessee",
+            answerC: "C. a four-story Georgian-style home in Missouri",
+            answerD: "D. a three story brick house in Arkansas"
+        }
+    },
+    five: {
+        question: "Which of the following items was owned by the fewest U.S. homes in 1990?",
+        correctAnswer: "B. CD player",
+        answers: {
+            answerA: "A. home computer",
+            answerB: "B. CD player",
+            answerC: "C. cordless phone",
+            answerD: "D. dishwasher"
+        }
+    },
+    six: {
+        question: "Who holds the record for the most victories in a row on the professional golf tour?",
+        correctAnswer: "C. Byron Nelson",
+        answers: {
+            answerA: "A. Jack Nicklaus",
+            answerB: "B. Arnold Palmer",
+            answerC: "C. Byron Nelson",
+            answerD: "D. Ben Hogan"
+        }
+    },
+    seven: {
+        question: "Who is third behind Hank Aaron and Babe Ruth in major league career home runs?",
+        correctAnswer: "C. Willie Mays",
+        answers: {
+            answerA: "A. Reggie Jackson",
+            answerB: "B. Harmon Killebrew",
+            answerC: "C. Willie Mays",
+            answerD: "D. Frank Robinson"
+        }
+    },
+    eight: {
+        question: "In 1990, in what percentage of U.S. married couples did the wife earn more money than the husband?",
+        correctAnswer: "B. 18%",
+        answers: {
+            answerA: "A. 8%",
+            answerB: "B. 18%",
+            answerC: "C. 38%",
+            answerD: "D. 58%"
+        }
+    },
+    nine: {
+        question: "During the 1980s for six consecutive years what breed of dog was the most popular in the U.S.?",
+        correctAnswer: "A. cocker spaniel",
+        answers: {
+            answerA: "A. cocker spaniel",
+            answerB: "B. German shepherd",
+            answerC: "C. Labrador retriever",
+            answerD: "D. poodle"
+        }
+    },
+    ten: {
+        question: "In 1985, five percent of U.S. households had telephone answering machines. By 1990 what percentage of homes had answering machines?",
+        correctAnswer: "C. 31%",
+        answers: {
+            answerA: "A. 10%",
+            answerB: "B. 11%",
+            answerC: "C. 31%",
+            answerD: "D. 51%"
+        }
+    },
+    eleven: {
+        question: "The first black American pictured on a U.S. postage stamp was who?",
+        correctAnswer: "D. Joe Louis",
+        answers: {
+            answerA: "A. Frederick Douglass",
+            answerB: "B. Booker T. Washington",
+            answerC: "C. Louis Armstrong",
+            answerD: "D. Joe Louis"
+        }
+    },
+    twelve: {
+        question: "What did the 'D' in 'D-Day' stand for?",
+        correctAnswer: "B. day",
+        answers: {
+            answerA: "A. doom",
+            answerB: "B. day",
+            answerC: "C. Dwight (Eisenhower)",
+            answerD: "D. Dunkirk"
+        }
+    },
+    thirteen: {
+        question: "The Brownie Box Camera introduced by Eastman Kodak in 1900 had a retail price of what?",
+        correctAnswer: "A. $1",
+        answers: {
+            answerA: "A. $1",
+            answerB: "B. $5",
+            answerC: "C. $10",
+            answerD: "D. $20"
+        }
+    },
+    fourteen: {
+        question: "Which of these characters turned 40 years old in 1990?",
+        correctAnswer: "A. Charlie Brown",
+        answers: {
+            answerA: "A. Charlie Brown",
+            answerB: "B. Bugs Bunny",
+            answerC: "C. Mickey Mouse",
+            answerD: "D. Fred Flintstone"
+        }
+    },
+    fifteen: {
+        question: "The Philadelphia mint started putting a 'P' mint mark on quarters when?",
+        correctAnswer: "B. 1980",
+        answers: {
+            answerA: "A. 1960",
+            answerB: "B. 1980",
+            answerC: "C. 1990",
+            answerD: "D. never"
+        }
+    },
+    sixteen: {
+        question: "Before becoming George Bush's Secretary of Defense, what was Dick Cheney's position?",
+        correctAnswer: "A. congressman from Wyoming",
+        answers: {
+            answerA: "A. congressman from Wyoming",
+            answerB: "B. governor of New Hampshire",
+            answerC: "C. secretary of defense under Ronald Reagan",
+            answerD: "D. business owner"
+        }
+    },
+    seventeen: {
+        question: "When Mt. St. Helens erupted on May 18, 1980, how many people were killed?",
+        correctAnswer: "B. 57",
+        answers: {
+            answerA: "A. 1",
+            answerB: "B. 57",
+            answerC: "C. 571",
+            answerD: "D. 5714"
+        }
+    },
+    eighteen: {
+        question: "In J. Edgar Hoover, what did the J stand for?",
+        correctAnswer: "B. John",
+        answers: {
+            answerA: "A. James",
+            answerB: "B. John",
+            answerC: "C. Jonathan",
+            answerD: "D. Joseph"
+        }
+    },
+    nineteen: {
+        question: "Florence Nightingale became known as 'the Lady With the Lamp' during which war?",
+        correctAnswer: "B. Crimean War",
+        answers: {
+            answerA: "A. American Civil War",
+            answerB: "B. Crimean War",
+            answerC: "C. World War I",
+            answerD: "D. Korean War"
+        }
+    },
+    twenty: {
+        question: "What year was it that the Census Bureau first reported that a majority of new mothers were remaining in the new job market?",
+        correctAnswer: "D. 1988",
+        answers: {
+            answerA: "A. 1958",
+            answerB: "B. 1968",
+            answerC: "C. 1978",
+            answerD: "D. 1988"
+        }
+    },
+}
